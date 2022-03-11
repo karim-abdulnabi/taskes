c1af350fbc7deb1bf8445682e764105abba5b3f6
karim abdulnabi <karimabdulnabi999@gmail.com>
2022-03-11 20:38:35 +0200

    for loop enfinety in python

---
+++ loop-enfinety.py
@@ -0,0 +1,23 @@
+
+
+def v():
+    while True:
+        print ("......")
+        print ("........")
+        print ("....")
+        print ("..........")
+        print (".......")
+
+
+for i in v() :
+    v()
+    
+    
+
+
+
+
+
+
+
+
