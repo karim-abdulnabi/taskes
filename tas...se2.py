65b731c16cc892e423ef0cd69ba2865aa532ebe0
karim abdulnabi <karimabdulnabi999@gmail.com>
2022-03-11 19:53:08 +0200

    delete the repetition in list

---
+++ tas...se2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 19:35:56 2022
+
+@author: USER_PC_SA
+"""
+
+a = ["karim","ahmed","mido","mona","karim","karim",]
+
+
+
+x = set(a)
+print (x)
+
+
+
